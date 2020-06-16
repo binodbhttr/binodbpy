a5f230e91e5a41ae9b40cb311cb326bf536201dd
Binod Bhattarai <binod.bhattarai@hotmail.com>
2020-06-16 16:15:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Binod Bhattarai',                   # Type in your name
   author_email = 'info@binodb.com.np',      # Type in your E-Mail
   url = 'https://github.com/binodbhttr/binodpy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/binodbhttr/binodpy/archive/0_2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/binodbhttr/binodpy/archive/0_3.tar.gz',    # I explain this later on
   keywords = ['Example', 'Samplepip', 'Pipexample'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
